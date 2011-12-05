b82ca50883d7fa3a52abc6b7a30ceba274405a44
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-12-05 13:58:42 +0100

    Fixed copyright headers.

--- src/main/scala/name/heikoseeberger/sbtsrc/package.scala
+++ src/main/scala/name/heikoseeberger/sbtsrc/package.scala
@@ -1,7 +1,5 @@
 /*
- * Copyright 2011 Typesafe Inc.
- * 
- * This work is based on the original contribution of WeigleWilczek.
+ * Copyright 2011 Heiko Seeberger
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
